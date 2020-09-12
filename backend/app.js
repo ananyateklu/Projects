496a4aae455d267ebd74b40b98060287361222ae
Ananya Teklu <teklu009@morris.umn.edu>
2020-09-12 13:27:25 -0500

    Create a simple database

--- backend/app.js
+++ backend/app.js
@@ -2,12 +2,22 @@ const Joi = require('joi');
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 const port = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true}
+);
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log("MongoDB successfully connected");
+})
 
 
 app.get('/', function(req, res) {
