9ca491b1ca3236e2838591c9593485cb3fbccf69
Ananya Teklu <teklu009@morris.umn.edu>
2020-09-12 18:56:52 -0500

    Require the routes to use them.

--- backend/app.js
+++ backend/app.js
@@ -11,6 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+//Create a connection with the database
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true}
 );
@@ -24,7 +25,14 @@ app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 
 });
+//Use the routes here 
+const exercisesRouter = require('./routes/exercises');
+const usersRouter = require('./routes/users');
 
+app.use('/exercises', exercisesRouter);
+app.use('/users', usersRouter);
+
+//server listens to the given port
 app.listen(port, function(error) {
     if(error) {
         console.log('Something went wrong', error)
