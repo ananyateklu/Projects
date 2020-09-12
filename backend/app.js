11c48a5c00d91c5d2e13a686e4dfcb059500c262
Ananya Teklu <teklu009@morris.umn.edu>
2020-09-12 11:58:32 -0500

    Remove all backend features for easy start.

--- backend/app.js
+++ backend/app.js
@@ -4,75 +4,13 @@ const port = 3000
 const app = express();
 const cors = require('cors');
 const path = require('path');
+
 app.use(express.json());
 
-const courses = [
-    {id: 1, name: 'course1'},
-    {id: 2, name: 'course2'},
-    {id: 3, name: 'course3'}
-]
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 
-});
-
-app.get('/api/courses', (req, res) => {
-    res.send(courses);
-
-});
-
-app.get('/api/courses/:id', (req, res) => {
-   const course = courses.find(c => c.id === parseInt(req.params.id));
-    if (!course) res.status(404).send('the couse with the given ID is not found');
-    res.send(course);
-
-});
-
-app.post('/api/courses', (req, res) => {
-    const { error } = validateCourse(req.body)
-    if (error) {
-        res.status(400).send(result.error.details[0].message);
-        return;
-    }
-    
-    const course = {
-        id: courses.length + 1,
-        name: req.body.name
-    };
-    courses.push(course);
-    res.send(course);
-});
-
-app.put('/api/courses/:id', (req, res) => {
-
-    const course = courses.find(c => c.id === parseInt(req.params.id));
-    if (!course) {
-        res.status(404).send('the couse with the given ID is not found');
-        return;
-    }
-
-    const { error } = validateCourse(req.body)
-    if (error) {
-        res.status(400).send(error.details[0].message);
-        return;
-    }
-    //Update course
-    course.name = req.body.name;
-    res.send(course);
-    //Return the updated course
-});
-
-app.delete('/api/courses/:id', (req, res) => {
-
-    const course = courses.find(c => c.id === parseInt(req.params.id));
-    if (!course) res.status(404).send('the couse with the given ID is not found');
-
-    //Deletee
-    const index = courses.indexOf(course);
-    courses.splice(index, 1);
-
-    res.send(course);
 });
 
 app.listen(port, function(error) {
@@ -82,11 +20,3 @@ app.listen(port, function(error) {
         console.log('Server is listening on port ' + port)
     }
 })
-
-function validateCourse(course) {
-    const schema = {
-        name: Joi.string().min(3).required()
-    };
-    
-    return Joi.validate(course, schema);
-}
