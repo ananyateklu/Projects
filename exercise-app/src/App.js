c12d5fb2663deea17ed856cdd5ae1453b99df3e5
Ananya Teklu <teklu009@morris.umn.edu>
2020-09-14 13:59:44 -0500

    integrating front end

---
+++ exercise-app/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+// import { BrowserRouter as Router, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css"
+
+
+function App() {
+  return (
+  <h1>hi</h1>
+  );
+}
+
+export default App;
