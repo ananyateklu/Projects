5ebc0b1934f79071a73f1258ec1559fd2ef64ba3
Ananya Teklu <teklu009@morris.umn.edu>
2020-09-17 12:07:31 -0500

    Simple style change

--- exercise-app/src/components/navbar.component.js
+++ exercise-app/src/components/navbar.component.js
@@ -5,7 +5,7 @@ export default class Navbar extends Component {
 
     render() {
         return (
-            <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
+            <nav className="navbar navbar-dark bg-dark navbar-expand-md">
                 <Link to="/" className="navbar-brand">ExerciseTrack</Link>
                 <div className="collapse navbar-collapse">
                 <ul className="navbar-nav mr-auto">
